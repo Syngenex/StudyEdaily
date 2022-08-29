f2a80fa3c35e87de1e3c487124bd8d5373d78066
Syngenex <17706325479@163.com>
2022-08-29 21:38:09 +0800

    增加多账号

--- src/main/java/daily.java
+++ src/main/java/daily.java
@@ -6,20 +6,38 @@ import java.util.List;
 
 public class daily {
 
-    private static String num="************";       //账号
-    private static String psd="******";     //密码
-    public static List<String> Cookie=null;       //cookie，自动获取不用填
-    private static String scopeKey="********";      //scopeKey
-    private static String entityKey="********";     //entityKey
-    private static String address="**省**市**区**村#经度值#纬度值";       //位置信息，经纬度值小数点后六位
+    private static List<String> num=List.of(new String[]{"num1","num2"});     //账号
+    private static List<String> psd=List.of(new String[]{"psd1","psd2"});    //密码
+    private static List<String> scopeKey=List.of(new String[]{"***","***"});      //scopeKey,自行抓包获取
+    private static List<String> entityKey=List.of(new String[]{"***","***"});     //entityKey,自行抓包获取
+    private static List<String> address=List.of(new String[]{"**省**市**区**村#经度值#纬度值", "**省**市**区**村#经度值#纬度值"});       //位置信息，经纬度值小数点后六位
+    private static String pushPlusKey="3d515bc0bb14410dba242b1b2c989f87";   //PushPlus推送Token
+    public static List<String> Cookie=null;      //cookie自动获取，不用填写
+    private static String failNum="";     //签到失败账号，不用填写
+
 
     public static void main(String[] args) throws UnsupportedEncodingException {
-        Cookie=Login(num,psd);
-        System.out.println("签到返回："+Checked(scopeKey,entityKey,address,Cookie));
+        for (int i=0; i<num.size(); i++){
+            Cookie=null;
+            System.out.println((i+1)+".准备签到"+":"+num.get(i));
+            Cookie=Login(num.get(i),psd.get(i));
+            if (Cookie==null){//登录失败
+                System.out.println(num.get(i)+"登录失败");
+                failNum=failNum+(num.get(i)+"，");
+            }else{//登录成功
+                Checked(scopeKey.get(i),entityKey.get(i),address.get(i),Cookie);
+                System.out.println("签到成功"+(i+1)+num.get(i)+"Success");
+            }
+        }
+        if (failNum!=""){
+            PushToPushPluse(pushPlusKey,"失败账号："+failNum.toString());
+        }else{
+            PushToPushPluse(pushPlusKey,"运行成功，为确保漏签请到APP查看是否成功签到");
+        }
     }
 
     private static List Login(String num, String psd) throws UnsupportedEncodingException {
-        final Base64.Encoder encoder = Base64.getEncoder();;
+        final Base64.Encoder encoder = Base64.getEncoder();
         final byte[] textByte = psd.getBytes("UTF-8");
         final String encod_psd = encoder.encodeToString(textByte);
 
@@ -37,17 +55,17 @@ public class daily {
             Response response=call.execute();
             String str =response.body().string();
             //判断是否登陆成功
-            if (str.indexOf("true")>=0){//登录成功
-                System.out.println("返回体："+str);
-                //adapter.name =str.substring(str.indexOf("userName\":\"")+11,str.indexOf("\",\"userKey\""));
+            if (str.contains("true")){
+                //获取cookie
+                if (response.isSuccessful()){
+                    Headers headers=response.headers();
+                    cookie= headers.values("Set-Cookie");
+                    System.out.println("Cookie"+cookie);
+                }
+                System.out.println(str.substring(str.indexOf("userName\":\"")+11,str.indexOf("\",\"userKey\""))+"登陆成功");
             }else {
-                System.out.println("登录失败");
-            }
-            //获取cookie
-            if (response.isSuccessful()){
-                Headers headers=response.headers();
-                cookie= headers.values("Set-Cookie");
-                System.out.println("Cookie"+cookie);
+                cookie=null;
+                //System.out.println("登录失败");
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -56,7 +74,7 @@ public class daily {
         return cookie;
     }
 
-    private static String Checked(String scopeKey,String entityKey,String address,List<String> Cookie){
+    private static void Checked(String scopeKey,String entityKey,String address,List<String> Cookie){
         OkHttpClient okHttpClient=new OkHttpClient();
         FormBody formBody =new FormBody.Builder()
                 .add("scopeKey",scopeKey)
@@ -71,14 +89,31 @@ public class daily {
         Call call=okHttpClient.newCall(request);
         try {
             Response response=call.execute();
-            //返回体
             String str =response.body().string();
-            return str;
-
+            System.out.println("签到成功:"+str);
         } catch (IOException e) {
             e.printStackTrace();
-            System.out.println("未知错误");
-            return e.toString();
+            System.out.println("未知错误:"+e);
+        }
+    }
+
+    private static void PushToPushPluse(String pushPlusKey,String sayToPush){
+        OkHttpClient okHttpClient=new OkHttpClient();
+        FormBody formBody =new FormBody.Builder()
+                .add("token",pushPlusKey)
+                .add("content",sayToPush).build();
+        Request request =new Request.Builder()
+                .url("http://www.pushplus.plus/send")
+                .post(formBody)
+                .build();
+        Call call=okHttpClient.newCall(request);
+        try {
+            Response response=call.execute();
+            String str =response.body().string();
+            System.out.println(str);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("推送错误:"+e);
         }
     }
 
